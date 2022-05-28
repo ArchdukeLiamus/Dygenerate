861ae09d389b624861540fa8ad84472722a1377e
ArchdukeLiamus <26291861+ArchdukeLiamus@users.noreply.github.com>
2022-05-28 00:10:42 -0400

    Fix descriptor typo in example

--- src/main/java/me/archdukeliamus/dygenerate/package-info.java
+++ src/main/java/me/archdukeliamus/dygenerate/package-info.java
@@ -195,7 +195,7 @@
  * {@literal @}InvokeDynamic(
  * 	"""	
  * 	invokestatic com/example/BootstrapClass.bootstrapMethod:
- * 	(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;ILFDLjava/lang/String;Ljava/lang/Class;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/Object;)Ljava/lang/invoke/CallSite;
+ * 	(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;IJFDLjava/lang/String;Ljava/lang/Class;Ljava/lang/invoke/MethodType;Ljava/lang/invoke/MethodHandle;Ljava/lang/Object;)Ljava/lang/invoke/CallSite;
  *	{
  *	  12345,
  *	  0xFFFFFFFF00000000L,
