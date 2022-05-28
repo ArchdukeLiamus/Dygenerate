e0bf085b26a5a911c181236fe49196044629e208
ArchdukeLiamus <26291861+ArchdukeLiamus@users.noreply.github.com>
2022-05-28 00:17:54 -0400

    Update docs

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.archdukeliamus.dygenerate"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.archdukeliamus.dygenerate"},{"l":"me.archdukeliamus.dygenerate.rtutils"}];updateSearchResults();
